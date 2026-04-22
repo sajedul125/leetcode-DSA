bcecf93654b6f599f08c8460d44b1d8c7f62e435
sajedul125 <sajedulislam756611@gmail.com>
2026-04-22 23:32:51 +0600

    Time: 2 ms (67.19%) | Memory: 23.5 MB (50.98%) - LeetSync

---
+++ 155-min-stack/min-stack.cpp
@@ -0,0 +1,46 @@
+class MinStack {
+public:
+stack<int>st,min_st;
+    MinStack() {  
+    }
+    
+    void push(int val) {
+
+        st.push(val);
+
+        if(min_st.empty())
+        {
+            min_st.push(val);
+        }
+        else if(min_st.top()>=val)
+        {
+            min_st.push(val);
+        }
+        
+        
+    }
+    
+    void pop() {
+    
+       
+        if(min_st.top()==st.top())
+        {
+             min_st.pop();
+            
+           
+        }
+        st.pop();
+        
+    }
+    
+    int top() {
+        return st.top();
+        
+    }
+    
+    int getMin() {
+        return min_st.top();
+        
+    }
+};
+
