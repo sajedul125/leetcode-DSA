afec9be8331f4b9e594c20d7d8579e53a83eb4d1
sajedul125 <sajedulislam756611@gmail.com>
2026-04-22 20:27:35 +0600

    Add files via upload

---
+++ reverse_queue.cpp
@@ -0,0 +1,19 @@
+//coding ninjas/code 360
+// Reversing a Queue
+
+#include <bits/stdc++.h> 
+queue<int> reverseQueue(queue<int> q)
+{
+   stack<int>st;
+   while(!q.empty())
+   {
+       st.push(q.front());
+       q.pop();
+   }
+   while(!st.empty())
+   {
+       q.push(st.top());
+       st.pop();
+   }
+   return q;
+}
